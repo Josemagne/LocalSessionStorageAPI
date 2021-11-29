25c0bbfd94efcfecfc56ddb1c1f942adefd6b0b9
Josemagne <jaime-barakat@hotmail.com>
2021-11-29 12:29:13 +0100

    changed site to visit to www.google.com

--- cypress/integration/create_item.ts
+++ cypress/integration/create_item.ts
@@ -1,7 +1,7 @@
 // import LocalSessionStorageAPI from "../../LocalSessionStorageAPI";
 describe("should have items in localStorage", () => {
   beforeEach(() => {
-    cy.visit("http://localhost");
+    cy.visit("https://www.google.com");
   });
   it("should have 1name be equal to Orlando", () => {
     localStorage.setItem("1persons", "1");
